7b51fe04e082b8d6a629f24975f5826aa434e0bc
lina <linawalid633@gmail.com>
2026-08-12 02:29:58 +0300

    how to find the click position

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const canvas = document.getElementById("invitationCanvas");
+const ctx = canvas.getContext("2d");
 const input = document.getElementById("invitationInput");
 const image = document.getElementById("invitationPreview");
 
@@ -12,6 +14,39 @@ input.addEventListener("change", function () {
     const imageURL = URL.createObjectURL(file);
 
     image.src = imageURL;
+});
+
+let namePositionX = null;
+let namePositionY = null;
+
+const positionText = document.getElementById("positionText");
+
+image.addEventListener("click", function(event) {
+
+     const rect = image.getBoundingClientRect();
+
+     const x = event.clientX - rect.left;
+     const y = event.clientY - rect.top;
+
+     const originalWidth = image.naturalWidth;
+     const originalHeight = image.naturalHeight;
+
+     const scaleX = originalWidth / rect.width;
+     const scaleY = originalHeight / rect.height
+
+     const originalX = x * scaleX;
+     const originalY = y * scaleY;
+
+     namePositionX = originalX;
+     namePositionY = originalY;
+
+     postionText.textContent =
+     `Original position: X = ${Math.round(originalX)}, Y = ${Math.round(originalY)}`;
+
+
+     console.log("Original X:", originalX);
+     console.log("Original Y:", originalY);
+
 });
 
 const csvInput = document.getElementById("csvInput");
@@ -46,3 +81,4 @@ csvInput.addEventListener("change", function () {
     });
 
 });
+
